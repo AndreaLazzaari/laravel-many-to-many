6d1f1b94bb58b1154cf883d294cf8c2ab544b9a2
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-02-20 17:26:47 +0100

    implementato seeder della Technology nel db

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +13,32 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $technologies= array(
+            array(
+              'technology'=> 'html'
+            ),
+            array(
+                'technology'=> 'css'
+              ),
+              array(
+                'technology'=> 'js'
+              ),
+              array(
+                'technology'=> 'vue'
+              ),
+              array(
+                'technology'=> 'php'
+              ),
+              array(
+                'technology'=> 'laravel'
+              ),
+          );
+
+    foreach ($technologies as $technology) {
+        $newTechnology = New Technology();
+        $newTechnology->technology = $technology['technology'];
+        $newTechnology->save();
     }
+}
 }
